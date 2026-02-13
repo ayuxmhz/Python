94f510741d5e23fec6213dbbc9e93d10e3d0cbc4
ayuxmhz <aayujmhz020@gmail.com>
2026-02-13 15:12:42 +0545

    Use of Strings

---
+++ Strings.py
@@ -0,0 +1,12 @@
+character_name="Zoro loves robin"
+print(len(character_name[0:5:1]))
+print(len(character_name))
+print(character_name.index("i"))
+print(character_name.replace("robin","Luffy"))
+character_age= 24
+print( character_name+ f" can't find his way back home. He will be ",character_age, " this year")
+# character_name = "Nico"
+# character_age="27"
+# print(F"{character_name} uses swords to fight and cut things down. she is happy she is {character_age} now") 
+# print("Yo homie\nwhat's up")
+# print("Python is fun to learn "+ character_name+" said that")
