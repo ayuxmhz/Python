01d5eb2c200fb8c6853483135bee88e9f7493857
ayuxmhz <aayujmhz020@gmail.com>
2026-02-13 15:07:13 +0545

    Some useful functions

---
+++ Int.py
@@ -0,0 +1,13 @@
+# from math import *
+import math
+
+# my_num = -10
+# print(str(my_num) + "$ Dollar")
+# print(abs(my_num))
+# print(pow(4, 2))
+# print(max(8, 0.100))
+print(min(8.999999, 2))
+print(round(9.13939, 1))
+print(math.floor(4.5))
+print(math.ceil(4.5))
+print(math.sqrt(25))
