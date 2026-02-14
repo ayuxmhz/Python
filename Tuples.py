8e7923bb1252628ac90ab57b182787791a054200
ayuxmhz <aayujmhz020@gmail.com>
2026-02-14 20:32:18 +0545

    slightly change

--- Tuples.py
+++ Tuples.py
@@ -1,4 +1,8 @@
-number=[(5,7,8,10) ,(20)]#immutable value the can't be changed or modify # if we put tuple inside the list then tuples index  works differently before (5,7,8,10) print(number[0]) gave 5 after added after inside the list the tuple [(5,7),(20)]=(5,7) are both in index 0 
-# number[0]=2#it will give error / can't be change 
-print(number[1])#output as we put the index right output = 5 at index 0 
-print(number)#useful for immutable 
+
+number = [
+    (5, 7, 8, 10),
+    (20),
+]  # immutable value the can't be changed or modify # if we put tuple inside the list then tuples index  works differently before (5,7,8,10) print(number[0]) gave 5 after added after inside the list the tuple [(5,7),(20)]=(5,7) are both in index 0
+# number[0]=2#it will give error / can't be change
+print(number[1])  # output as we put the index right output = 5 at index 0
+print(number)  # useful for immutable
