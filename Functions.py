c234021dcf8209358d1615fa40ab611c82300e0d
ayuxmhz <aayujmhz020@gmail.com>
2026-02-15 00:18:28 +0545

    Explained functions

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
-def momo(name,type):
+def momo(name,type):# we created a function using def and name the function momo and we passed the parameter 
     # print("Momo says hi")
-    print("Do you love " + name +" She is "+type)
+    print("Do you love " + name +" She is "+type)#Here we used those two parameters to print out what we want 
 
 
-momo("Nami","Hot")
-momo("Nico","Cool")
+momo("Nami","Hot")# we called the momo function and we gave value to the parameters , we can pass any value 
+momo("Nico","Cool")# we called the momo function and we gave value to the parameters 
