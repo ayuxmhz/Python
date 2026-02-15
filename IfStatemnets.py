f8294a9ae66de79123845aa60871c9a8406eb3ce
ayuxmhz <aayujmhz020@gmail.com>
2026-02-15 15:57:48 +0545

    Comparison operator in If statements

--- IfStatemnets.py
+++ IfStatemnets.py
@@ -15,14 +15,33 @@
 #     print("You can vote")
 # else:
 #     print("You can't vote")
-is_male = False
-is_tall = True
+# is_male = False
+# is_tall = True
 
-if is_male and is_tall:
-    print("You are a tall male")
-elif is_male or not (is_tall):
-    print("You/sandy are not a tall man")
-elif not (is_male) and is_tall:
-    print("You are not male but tall")
-else:
-    print("You are neither male nor tall")
+# if is_male and is_tall:# checks user is male and tall if condition becomes True it gets print out
+#     print("You are a tall male")
+# elif is_male or not (is_tall):#Checks user is male but not tall if condition matches It gets print
+#     print("You/sandy are not a tall man")
+# elif not (is_male) and is_tall:#check user is not male but tall if condition matches it gets printed
+#     print("You are not male but tall")#Condition matches so this gets printed
+# Note: and is a logical operator  , both conditions needs to be True to be printed , If one is False & another is True it doesn't prints | False and False never prints
+# or is the opposite of and operator    at least one condition has to be True  | False or False never prints
+# else:
+#     print("You are neither male nor tall")
+
+num1 = int(input("Enter the number 1: "))
+num2 = int(input("Enter the number 2: "))
+num3 = int(input("Enter the number 3: "))
+
+
+def max_num(num1, num2, num3):
+
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(num1, num2, num3))
