707801cbd0b80f1a5d364ee401e5b7814530484a
ayuxmhz <aayujmhz020@gmail.com>
2026-02-15 14:55:57 +0545

    comment down

---
+++ Practice.py
@@ -0,0 +1,14 @@
+# num1 = input("Enter the first number: ")
+# num2 = input("Enter the second number: ")
+# print(int(num1) + int(num2))
+# print(int(num1) * int(num2))
+# print(int(num1) - int(num2))
+# print(int(num1) / int(num2))
+
+# result =int(num1+num2)# so first the string does the concatenation then it turn it to int so no addition is between
+
+num = int(input("Enter the number: "))
+
+
+print(pow(num, 2))
+print(pow(num, 3))
--- TakeInput.py
+++ TakeInput.py
@@ -3,12 +3,12 @@
 # print("Hello " + name + " How are you?")
 # print("Your age is " + age + "!!")
 
-# nub1 = input("Enter a number: ")
-# nub2 = input("Enter another number: ")
-# result = float(nub1) + float(
-#     nub2
-# )  # int function  is a full number, we can't add int and float together using int function so we use float function now we can add floatnumber 4.5 and int number 5 = 9.5
-# print(result)
+nub1 = input("Enter a number: ")
+nub2 = input("Enter another number: ")
+result = float(nub1) + float(
+    nub2
+)  # int function  is a full number, we can't add int and float together using int function so we use float function now we can add floatnumber 4.5 and int number 5 = 9.5
+print(result)
 
 # print(type(10))
 # print(type(90.0))
