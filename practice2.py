250a6b3cd557a9cfa32c3643a9f0995196c58258
ayuxmhz <aayujmhz020@gmail.com>
2026-02-19 00:46:52 +0545

    More practice

--- practice2.py
+++ practice2.py
@@ -1,3 +1,4 @@
+# Task 1
 # num = 1
 # user_number = int(input("Enter the number: "))
 
@@ -5,6 +6,7 @@
 #     print(num)
 #     num += 1
 
+# Task 2
 # number = 1
 # user_num = int(input("Enter the number: "))
 
@@ -12,7 +14,7 @@
 #     print(user_num)
 #     user_num -= 1
 
-
+# Task 3
 # ur_number = int(input("Enter the number: "))
 # num = 1
 # total_sum = 0
@@ -24,14 +26,35 @@
 
 # print(f" The sum from 1 to {ur_number} is: {total_sum}")
 
+# Task 4
+# password = "python123"
+# user_password = " "
 
-password = "python123"
-user_password = " "
+
+# while user_password != password:
+#     user_password = input("Enter the password: ")
+#     if user_password == password:
+#         print("Access Granted!")
+#     else:
+#         print("Access denied!,Try again!! \n")
 
 
-while user_password != password:
-    user_password = input("Enter the password: ")
-    if user_password == password:
-        print("Access Granted!")
+# Task 5
+
+secret_number = 99
+user_number = " "
+tries = 0
+total_tires = 3
+out_of_tries = False
+
+while user_number != secret_number and not (out_of_tries):
+    if tries < total_tires:
+        user_number = int(input("Enter the number: "))
+        tries += 1
     else:
-        print("Access denied!,Try again!! \n")
+        out_of_tries = True
+
+if out_of_tries:
+    print("Sorry!! You are out of tries, Try next time goodbye to you money")
+else:
+    print("Congrats you have guess the winning number")
