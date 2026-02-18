6ed99f205ad5d8ff916e5dc1b734f0803e185963
ayuxmhz <aayujmhz020@gmail.com>
2026-02-18 19:04:05 +0545

    For loops

---
+++ Forloop.py
@@ -0,0 +1,12 @@
+# for letter in "One piece": #prints  one piece letters one by one 
+#   # print(letter)
+character_name = ["Nico", "Nami", "Orihime", "Ruika"] # we created list and we stored the list elements to character_name 
+for i in range(len(character_name)):# len(character_name) is 4 it is the length of list | so it run 4 time that is 0-3
+    print(i,character_name[i])#i=index so i in front give 0-3 and character_name[i] give the elements inside the list 
+
+
+# for i in range(10):# loop 0 t0 9 
+#     if i == 0:# if the i ==0 it prints this 
+#         print("Wow wow")
+#     else:# so this gets printed 9 times 
+#         print("Not wow")
